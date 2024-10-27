d7e52e4435e30f946048aad89a5bcf6b98ea1046
Slava Kostak <slavakostak2010@gmail.com>
2024-10-27 17:44:25 +0100

    init

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import { Container, Filters, Title, TopBar } from "@/components/shared";
+
+export default function Home() {
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="All pizzas" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+      <Container className="pb-14">
+        <div className="flex gap-[60px]">
+          {/* Filtering */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/* Products list */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Product list</div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
