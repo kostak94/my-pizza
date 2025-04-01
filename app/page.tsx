d8b81c1370789e78807055f82e15320f5bb32594
Slava Kostak <slavakostak2010@gmail.com>
2025-04-01 13:30:58 +0200

    components_update

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <Title text="All pizzas" size="lg" className="font-extrabold" />
       </Container>
       <TopBar />
-      <Container className="pb-14">
+      <Container className="mt-10 pb-14">
         <div className="flex gap-[60px]">
           {/* Filtering */}
           <div className="w-[250px]">
